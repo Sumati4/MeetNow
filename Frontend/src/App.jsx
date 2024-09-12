4762a70b28f78c1721a6a51d73a1c4cb1d4f1491
sumati4 <sumati3976@gmail.com>
2024-09-12 21:48:12 +0530

    landing page update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css'
 import LandingPage from './pages/landing'; 
+import Authentication from './pages/Authentication';
 
 function App() {
  
   return (
     <>
 <Router>
-  <Routes><Route path='/' element={<LandingPage/>}/></Routes>
+  <Routes><Route path='/' element={<LandingPage/>}/>
+           <Route path='/auth'element={<Authentication/>}/>
+  </Routes>
   
 </Router>
   </>
